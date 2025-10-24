b0d101f767a155634d854746adb455e835731c1e
Vinícius Ferreira Salge <104887873+vinicius-salge@users.noreply.github.com>
2025-10-24 20:31:28 +0000

    adiciona scripts `lint:check` e `lint:finx`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para estudo acompanhando o curso do Filipe Deschamps no https://curso.dev/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Oie, espero que tenha um ótimo dia! Apesar de todas as dificuldades ou obstáculos que aparecerem, você é forte, uma pessoa capaz e vai tudo dar certo e melhorar..</h1>
+  return (
+    <h1>
+      Oie, espero que tenha um ótimo dia! Apesar de todas as dificuldades ou
+      obstáculos que aparecerem, você é forte, uma pessoa capaz e vai tudo dar
+      certo e melhorar..
+    </h1>
+  );
 }
 
 export default Home;
